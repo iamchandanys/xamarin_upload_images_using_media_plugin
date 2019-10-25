e903440c46bc8352e17df386c8587ee5dd10b699
chandanys <chandan.kumar@udmglobal.com>
2019-10-25 12:29:48 +0530

    Added Complete Project

---
+++ UploadImages/UploadImages/UploadImages.Android/MainActivity.cs
@@ -0,0 +1,35 @@
+﻿using System;
+
+using Android.App;
+using Android.Content.PM;
+using Android.Runtime;
+using Android.Views;
+using Android.Widget;
+using Android.OS;
+using Plugin.CurrentActivity;
+using Plugin.Permissions;
+
+namespace UploadImages.Droid
+{
+    [Activity(Label = "UploadImages", Icon = "@mipmap/icon", Theme = "@style/MainTheme", MainLauncher = true, ConfigurationChanges = ConfigChanges.ScreenSize | ConfigChanges.Orientation)]
+    public class MainActivity : global::Xamarin.Forms.Platform.Android.FormsAppCompatActivity
+    {
+        protected override void OnCreate(Bundle savedInstanceState)
+        {
+            TabLayoutResource = Resource.Layout.Tabbar;
+            ToolbarResource = Resource.Layout.Toolbar;
+
+            base.OnCreate(savedInstanceState);
+            CrossCurrentActivity.Current.Init(this, savedInstanceState);
+            global::Xamarin.Forms.Forms.Init(this, savedInstanceState);
+            LoadApplication(new App());
+        }
+
+        public override void OnRequestPermissionsResult(int requestCode, string[] permissions, Android.Content.PM.Permission[] grantResults)
+        {
+            PermissionsImplementation.Current.OnRequestPermissionsResult(requestCode, permissions, grantResults);
+
+            base.OnRequestPermissionsResult(requestCode, permissions, grantResults);
+        }
+    }
+}
---
+++ UploadImages/UploadImages/UploadImages/MainPage.xaml.cs
@@ -0,0 +1,72 @@
+﻿using Plugin.Media;
+using Plugin.Media.Abstractions;
+using Plugin.Permissions;
+using Plugin.Permissions.Abstractions;
+using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+
+namespace UploadImages
+{
+    public partial class MainPage : ContentPage
+    {
+        ObservableCollection<MediaFile> files = new ObservableCollection<MediaFile>();
+
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+
+        private async void TakePhotoBtnClicked(object sender, EventArgs e)
+        {
+            if (!CrossMedia.Current.IsCameraAvailable || !CrossMedia.Current.IsTakePhotoSupported)
+            {
+                await DisplayAlert("No Camera", "No Camera Avaialble.", "OK");
+                return;
+            }
+            else
+            {
+                var cameraStatus = await CrossPermissions.Current.CheckPermissionStatusAsync(Permission.Camera);
+                var storageStatus = await CrossPermissions.Current.CheckPermissionStatusAsync(Permission.Storage);
+
+                if (cameraStatus != PermissionStatus.Granted || storageStatus != PermissionStatus.Granted)
+                {
+                    var cameraAndStorageRequest = await CrossPermissions.Current.RequestPermissionsAsync(new[] { Permission.Camera, Permission.Storage });
+
+                    cameraStatus = cameraAndStorageRequest[Permission.Camera];
+                    storageStatus = cameraAndStorageRequest[Permission.Storage];
+                }
+
+                if (cameraStatus == PermissionStatus.Granted && storageStatus == PermissionStatus.Granted)
+                {
+                    files.Clear();
+                    
+                    var file = await CrossMedia.Current.TakePhotoAsync(new StoreCameraMediaOptions
+                    {
+                        AllowCropping = true,
+                        PhotoSize = PhotoSize.Medium,
+                        Directory = "Sample",
+                        Name = "test.jpg"
+                    });
+
+                    if (file == null)
+                        return;
+
+                    files.Add(file);
+
+                    await DisplayAlert("File Location", "Pic Added Successfully!", "OK");
+                }
+                else
+                {
+                    await DisplayAlert("Permissions Denied", "Unable to take photos.", "OK");
+
+                    CrossPermissions.Current.OpenAppSettings();
+                }
+            }
+        }
+    }
+}
